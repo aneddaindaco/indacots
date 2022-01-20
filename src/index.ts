0f79ca62188352c1b314a5efb8b133eaab47e9c0
aneddaindaco <anedda@indacoproject.it>
2022-01-20 10:06:51 +0100

    Aggiunta lib pandoc

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,45 @@
+
+import { Pandoc, PandocOutFormat, PandocData } from "pandoc-ts";
+
+
+
 export class program{
     main():void{
-        console.log("ciao");
+        
+        const outs: PandocOutFormat[] = [
+            { name: "markdown", format: "gfm", outBin: false },
+            { name: "markdownFile", format: "gfm", fname: "sample.md" },
+            { name: "html", format: "html", fname: "sample.html" },
+          ];
+          
+          const pandocInstance = new Pandoc("docx", outs);
+          
+          pandocInstance.convertFile("test pandoc.docx", (result, err) => {
+            if (err) {
+              console.error(err);
+            }
+            if (result) {
+              console.log(result.markdown);
+              console.log(result.markdownFile);
+              console.log(result.html);
+            }
+          });
+    }
+
+    mainAsync():Promise<void>{
+        return new Promise<void>((Resolve, Reject)=>{
+            try{
+                this.main();
+                Resolve();
+            }
+            catch(e){
+                Reject(e);
+            }
+        });
     }
 }
+
+
 
 var p = new program();
 p.main();
