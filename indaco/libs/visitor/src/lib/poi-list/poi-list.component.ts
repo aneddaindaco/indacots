aa27b95463fabeeedc88348a1f14a6bcaf474243
aneddaindaco <anedda@indacoproject.it>
2022-02-11 10:05:33 +0100

    Primo commit

---
+++ indaco/libs/visitor/src/lib/poi-list/poi-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { PoiActions, PoiEntity, PoiSelectors } from '@indaco/poi';
+
+@Component({
+  selector: 'indaco-poi-list',
+  templateUrl: './poi-list.component.html',
+  styleUrls: ['./poi-list.component.css']
+})
+export class PoiListComponent implements OnInit {
+
+  pois$=this.store.select(PoiSelectors.getAllPoi)
+  constructor(private store: Store) { }
+
+  ngOnInit(): void {
+    this.store.dispatch(PoiActions.init())
+  }
+
+}
